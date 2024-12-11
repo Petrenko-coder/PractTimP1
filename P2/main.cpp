b2f4102bb0c98676dcb8fb71c60ec1a310ba063c
Petrenko-coder <hdncringer@gmail.com>
2024-12-11 07:18:13 +0000

    Update main.cpp

--- P2/main.cpp
+++ P2/main.cpp
@@ -1,47 +1,24 @@
 #include <stdio.h>
 #include <iostream>
-#include <cctype>
-#include "BetaCipher.h"
-#include <locale>
+#include <string>
+#include "/home/student/my_cplusplus_programs/Labtimp/Labtimp1/Zadanie2/program2/chifr.h"
 using namespace std;
-bool isValid(const wstring& s)
-{
-     for(auto c:s)
-         if (!iswalpha(c) || !iswupper(c))
-             return false;
-     return true;
-}
+
+string encrypt(string input, const int key);
+string decrypt(string input, const int key);
+
 int main(int argc, char **argv)
 {
-    int key=3;
-    locale loc("ru_RU.UTF-8");
-    locale::global(loc);
-    wstring z;
-    wstring text;
-    unsigned op;
-    wcout<<L"Cipher ready. Input please Password: ";
-    wcin>>z;
-    BetaCipher cipher(key);
-    do {
-        wcout<<L"Cipher ready. Input please operation (0-exit, 1-encrypt, 2-decrypt): ";
-        wcin>>op;
-        if (op > 2) {
-            wcout<<L"Illegal operation\n";
-        } else if (op >0) {
-            wcout<<L"Cipher ready. Input please text: ";
-          wcin>>text;
-          if (isValid(text)) {
-              if (op==1) {
-                  wcout<<L"Encrypted text: 
-"<<cipher.CoderBetaCipher(cipher, text)<<endl;
-                     } else { 
-                       wcout<<L"Decrypted text: 
-"<<cipher.DecoderBetaCipher(cipher, text)<<endl;
-                      }
-                  } else {
-                      wcout<<L"Operation aborted: invalid text\n";
-                 }
-            }
-       } while (op!=0);
-       return 0;
+    Chifr encoder;  // Изменено имя на Chifr
+    const int COLS = 10;
+
+    string publicMessage = "GERALTISRIVII";  // Переименовано для улучшения читаемости
+    string privateMessage = encoder.encrypt(publicMessage, COLS);  // Изменено имя объекта на encoder
+    string decodedMessage = encoder.decrypt(privateMessage, COLS);  // Переименовано для согласованности
+
+    cout << publicMessage << endl;
+    cout << privateMessage << endl;
+    cout << decodedMessage << endl;
+
+    return 0;
 }
