0fa3b3d4967c664ca932fe6486feb41d1afb44ab
Petrenko-coder <hdncringer@gmail.com>
2024-12-11 07:13:11 +0000

    Update modAlphaCipher.h

--- P1/modAlphaCipher.h
+++ P1/modAlphaCipher.h
@@ -1,17 +1,24 @@
-       #include <map>
-       #include <locale>
-       using namespace std;
-       class modAlphaCipher
-       {
-       private:
-        wstring numAlpha =L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; //алфавит по порядку
-        map <char,int> alphaNum; //ассоциативный массив "номер по символу"
-        vector <int> key; //ключ
-        vector<int> convert(const wstring& s); //преобразование строка-вектор
-        wstring convert(const vector<int>& v); //преобразование вектор-строка
-       public:
-        modAlphaCipher()=delete; //запретим конструктор без параметров
-        modAlphaCipher(const wstring& skey); //конструктор для установки ключа
-        wstring encrypt(const wstring& open_text); //зашифрование
-        wstring decrypt(const wstring& cipher_text);//расшифрование
-       };
+    #pragma once
+
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class modAlphaCipher
+{
+private:
+    const wstring numAlpha = L"АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    map<wchar_t, int> alphaNum;
+    vector<int> key; // ключ
+
+    vector<int> convert(const wstring& s);
+    wstring convert(const vector<int>& v);
+
+public:
+    modAlphaCipher() = delete;
+    explicit modAlphaCipher(const wstring& skey);
+    wstring encrypt(const wstring& open_text);
+    wstring decrypt(const wstring& cipher_text);
+};
